567212c248fbb59b2e835888e3fade8e219e5dba
Anduin Xue <xuef@microsoft.com>
2022-11-12 07:28:14 +0000

    Remove azure content moderator.

--- src/Moonglade.Comments/ServiceCollectionExtensions.cs
+++ src/Moonglade.Comments/ServiceCollectionExtensions.cs
@@ -9,28 +9,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddComments(this IServiceCollection services, IConfiguration configuration)
     {
-        var section = configuration.GetSection("AzureContentModerator");
-        var settings = section.Get<AzureContentModeratorSettings>();
-
-        services.Configure<AzureContentModeratorSettings>(section);
-
-        if (settings != null &&
-            !string.IsNullOrWhiteSpace(settings.Endpoint) &&
-            !string.IsNullOrWhiteSpace(settings.OcpApimSubscriptionKey))
-        {
-            var cred = new ApiKeyServiceClientCredentials(settings.OcpApimSubscriptionKey);
-            services.AddTransient<IContentModeratorClient>(_ => new ContentModeratorClient(cred)
-            {
-                Endpoint = settings.Endpoint
-            });
-
-            services.AddScoped<ICommentModerator, AzureContentModerator>();
-        }
-        else
-        {
-            services.AddScoped<ICommentModerator, LocalWordFilterModerator>();
-        }
-
+        services.AddScoped<ICommentModerator, LocalWordFilterModerator>();
         return services;
     }
 }
