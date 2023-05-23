79fc1465c2b9b0b9d28dd82d1a6eb61871a55400
Anduin Xue <anduin@aiursoft.com>
2023-05-23 15:54:38 +0000

    UT try to call an API.

--- tests/Moonglade.Tests/StartUpTest.cs
+++ tests/Moonglade.Tests/StartUpTest.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,19 +10,42 @@ namespace MoongladePure.Tests;
 [TestClass]
 public class StartUpTest
 {
-    [TestMethod]
-    public async Task StartTest()
+    [CanBeNull] private WebApplication app;
+
+    [TestInitialize]
+    public async Task PrepareServer()
     {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
         var builder = WebApplication.CreateBuilder();
+
+        //builder.WebHost.UseUrls("http://0.0.0.0:61237");
 
         Program.ConfigureServices(builder.Services, builder.Configuration, isTest: true);
 
-        var app = builder.Build();
+        app = builder.Build();
 
         await Program.FirstRun(app);
 
         Program.ConfigureMiddleware(app);
 
         await app.StartAsync();
+    }
+
+    [TestCleanup]
+    public async Task CleanServer()
+    {
+        if (app != null)
+        {
+            await app.StopAsync();
+        }
+    }
+
+    [TestMethod]
+    public async Task StartTest()
+    {
+        var http = new HttpClient();
+        _ = await http.GetAsync("http://localhost:5000/admin");
+        //response.EnsureSuccessStatusCode(); // Status Code 200-299
     }
 }
