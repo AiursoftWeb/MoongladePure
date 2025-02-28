912ed38f6729ab9ae920b59c7d61fa704f91ed0f
Anduin Xue <anduin@aiursoft.com>
2025-02-28 02:18:33 +0000

    Revert old db first.

--- src/Moonglade.Data.MySql/MySqlBlogDbContext.cs
+++ src/Moonglade.Data.MySql/MySqlBlogDbContext.cs
@@ -2,10 +2,28 @@
 using Aiursoft.DbTools.MySql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MoongladePure.Data.MySql.Configurations;
 
 namespace MoongladePure.Data.MySql;
 
-public class MySqlContext(DbContextOptions<MySqlContext> options) : BlogDbContext(options);
+public class MySqlContext(DbContextOptions<MySqlContext> options) : BlogDbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CommentConfiguration());
+        modelBuilder.ApplyConfiguration(new CommentReplyConfiguration());
+        modelBuilder.ApplyConfiguration(new PostConfiguration());
+        modelBuilder.ApplyConfiguration(new PostCategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new PostExtensionConfiguration());
+        modelBuilder.ApplyConfiguration(new LocalAccountConfiguration());
+        modelBuilder.ApplyConfiguration(new BlogThemeConfiguration());
+        modelBuilder.ApplyConfiguration(new BlogAssetConfiguration());
+        modelBuilder.ApplyConfiguration(new BlogConfigurationConfiguration());
+        modelBuilder.ApplyConfiguration(new PageConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
 
 public class MySqlSupportedDb(bool allowCache, bool splitQuery) : SupportedDatabaseType<BlogDbContext>
 {
